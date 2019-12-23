e2e3948cc8628f6904f4096c8fa4da4e3419d39e
lgope <ilakshmangope@gmail.com>
2019-12-23 23:23:08 +0600

    Abstract Class Challenge 🗽📝

---
+++ AbstractClassChallenge/src/com/abstractClass/NodeList.java
@@ -0,0 +1,9 @@
+package com.abstractClass;
+
+public interface NodeList {
+        
+        ListItem getRoot();
+        boolean addItem(ListItem item);
+        boolean removeItem(ListItem item);
+        void traverse(ListItem root);
+}
